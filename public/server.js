5cb05ff6ba23c9ab53332446dc88f622598fcb57
PabloWinter <pablowinter7@hotmail.com>
2019-07-08 18:06:23 -0600

    Added routes for different national parks.

--- public/server.js
+++ public/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+
+
 //Set port
 app.set('port', process.env.PORT || 8000);
 
@@ -10,9 +12,31 @@ app.set('view engine', 'ejs');
 app.set('views', 'public/views');
 
 //Routes
+//home page
 app.get('/', (req, res) => {
     res.render('index', { title: 'Canada National Parks'});
-})
+});
+
+//Banff national park
+app.get('/banff-park', (req, res) => {
+    res.render('banff-park', { title: 'Banff National Park'});
+});
+
+//Jasper national park
+app.get('/jasper-park', (req, res) => {
+    res.render('jasper-park', { title: 'Jasper National Park'});
+});
+
+//Yoho national park
+app.get('/yoho-park', (req, res) => {
+    res.render('yoho-park', { title: 'Yoho National Park'});
+});
+
+//Cape breton national park
+app.get('/cape-breton-park', (req, res) => {
+    res.render('cape-breton-park', { title: 'Cape Breton National Park'});
+});
+
 
 //Server listening on port 8000
 app.listen(app.get('port'), () => {
